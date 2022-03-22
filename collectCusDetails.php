d9e34537d8dbaa761ec7e4dfeb4d8afcf1ce333a
Pasindu Kavishka <87638408+KavishkaPKP@users.noreply.github.com>
2022-03-23 01:37:02 +0530

    start Room Management

---
+++ collectCusDetails.php
@@ -0,0 +1,64 @@
+<meta charset="UTF-8">
+    <meta name="viewport"
+    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!-- CSS only -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"><link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+
+
+<title>Nidella Rest | Select Rooms</title>
+
+<!-- header -->
+
+	<div class="w3_navigation">
+		<div class="container">
+			<nav class="navbar navbar-expand-sm navbar-dark">
+				<div class="navbar-header navbar-left ">
+					<h1><a class="navbar-brand" href="index.php">NIDELLA <span>REST</span><p class="logo_w3l_agile_caption">Book Your Dream Resort</p></a></h1>
+				</div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span></button>
+					
+                    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <nav class="menu menu--iris">
+						<ul class="navbar-nav">
+							<li class="menu__item--current"><a href="#" class="menu__link ">Menu selection</a></li>
+							<br>
+							<li class="menu__item"><a href="#" class="menu__link ">Room Booking</a></li>
+							<li class="menu__item"><a href="#" class="menu__link ">Events Selection</a></li>
+							<li class="menu__item"><a href="#" class="menu__link ">Hall Selection</a></li>
+						</ul>
+					</nav>
+				</div>
+			</nav>
+		</div>
+	</div>
+</div>
+	<!-- end header -->
+
+
+	
+
+
+<h1>H</h1>
+<h1>E</h1>
+<h1>L</h1>
+<h1>L</h1>
+<h1>O</h1>
+
+
+
+<!--start footer-->
+<div class="copy">
+    <p>© 2022 NIDELLAREST . All Rights Reserved | Design by <a href="index.php">NIDELLAREST</a> </p>
+</div>
+<!-- endfooter-->
+
+<!-- Boostrup java scripts -->
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
